5f2b78d73742069af0b18760a8d2eb061892e304
Klaus Purer <klaus.purer@gmail.com>
2017-06-23 20:17:43 +0200

    refactor(proxy): Update Hyper library and rewrite server

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,55 @@
 extern crate hyper;
+extern crate futures;
+extern crate tokio_core;
 
 use hyper::Client;
-use hyper::client::IntoUrl;
-use hyper::server::{Server, Request, Response, Listening, Handler};
-use hyper::uri::RequestUri;
-use hyper::header::Host;
-use std::error::Error;
-use hyper::status::StatusCode;
-use std::io;
+use hyper::server::{Http, Request, Response, Service};
+use tokio_core::reactor::Core;
+use hyper::StatusCode;
 
-// We need to define a handler because we need to pass in an upstream port
-// number the HTTP client will connect to.
-struct ProxyHandler {
+struct Proxy {
     upstream_port: u16,
 }
 
-impl Handler for ProxyHandler {
-    fn handle(&self, request: Request, response: Response) {
-        pipe_through(request, response, self.upstream_port);
+impl Service for Proxy {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        let response = pipe_through(request, self.upstream_port);
+        futures::future::ok(response)
     }
 }
 
-pub fn start_server(port: u16, upstream_port: u16) -> Listening {
+pub fn start_server(port: u16, upstream_port: u16) {
     let address = "127.0.0.1:".to_owned() + &port.to_string();
-    let server = Server::http(&address).unwrap();
-    let listening = server
-        .handle(ProxyHandler { upstream_port: upstream_port })
-        .unwrap();
     println!("Listening on {}", address);
-
-    listening
+    let addr = address.parse().unwrap();
+    let server = Http::new()
+        .bind(&addr, move || Ok(Proxy { upstream_port }))
+        .unwrap();
+    server.run().unwrap();
 }
 
-fn pipe_through(request: Request, mut response: Response, upstream_port: u16) {
+fn pipe_through(request: Request, upstream_port: u16) -> Response {
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
 
+    let uri = "http://drupal-8.localhost".parse().unwrap();
 
-    let path = match request.uri {
-        RequestUri::AbsolutePath(p) |
-        RequestUri::Authority(p) => p,
-        RequestUri::AbsoluteUri(url) => url.path().to_string(),
-        RequestUri::Star => "*".to_string(),
-    };
-    let host = match request.headers.get::<Host>() {
-        None => {
-            return error_page("No host header in request",
-                              StatusCode::BadRequest,
-                              response)
+    let work = client.get(uri);
+
+    match core.run(work) {
+        // Directly pass through the client response.
+        Ok(response) => response,
+        Err(_) => {
+            let mut response = Response::new();
+            response.set_status(StatusCode::InternalServerError);
+            response.set_body("error");
+
+            response
         }
-        Some(h) => h,
-    };
-    let hostname = host.hostname.to_string();
-    // String concatenation is complicated in Rust. In order to create a new variable which
-    // concatenates 3 strings we first have to allocate memory by making the first variable a
-    // string.
-    let protocol = "http://".to_string();
-    let url_string = protocol + &hostname + ":" + &upstream_port.to_string() + &path;
-    let url = match url_string.into_url() {
-        Ok(u) => u,
-        Err(e) => {
-            return error_page(&format!("Error parsing Host header '{}': {}",
-                                       url_string,
-                                       e.description()),
-                              StatusCode::InternalServerError,
-                              response)
-        }
-    };
-
-    // @todo Add proxy config so that requests always go to localhost and this is not an open
-    // relay.
-    let client = Client::new();
-
-    let request_builder = client
-        .request(request.method, url)
-        .headers(request.headers.clone());
-    let mut upstream_response = request_builder.send().unwrap();
-    *response.status_mut() = upstream_response.status;
-    // Cloning is quite useless here, we actually just want to move the headers. But how?
-    *response.headers_mut() = upstream_response.headers.clone();
-
-    // Forward the body of the upstream response in our response body.
-    io::copy(&mut upstream_response, &mut response.start().unwrap()).unwrap();
-}
-
-/**
- * Sets an error response.
- */
-fn error_page(message: &str, http_code: StatusCode, mut response: Response) {
-    println!("{}", message);
-    *response.status_mut() = http_code;
-    // @todo set response body with the message.
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-// We always want to run Clippy on this code and have it as a dependency. Don't
-// do this for real Rust applications.
 #![deny(warnings)]
-#![feature(plugin)]
-#![plugin(clippy)]
 
 extern crate rustnish;
 
@@ -10,7 +6,5 @@ extern crate rustnish;
 fn main() {
     let port: u16 = 9090;
     let upstream_port: u16 = 80;
-    // If a function returns something in Rust you can't ignore it, so we need this superflous
-    // unused variable here. Starting it with "_" tells the compiler to ignore it.
-    let _listening = rustnish::start_server(port, upstream_port);
+    rustnish::start_server(port, upstream_port);
 }
