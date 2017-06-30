fee1bfa55cc874926eb9135647a16706f1921863
Klaus Purer <klaus.purer@gmail.com>
2017-06-30 19:20:56 +0200

    Working server/client with 1 tokio core!

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,27 @@ extern crate tokio_core;
 
 use hyper::Client;
 use hyper::server::{Http, Request, Response, Service};
-use tokio_core::reactor::Core;
-use hyper::StatusCode;
 use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
 use futures::Stream;
+use hyper::client::HttpConnector;
+use hyper::client::FutureResponse;
 
 struct Proxy {
     upstream_port: u16,
+    client: Client<HttpConnector>,
 }
 
 impl Service for Proxy {
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
-    type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+    type Future = FutureResponse;
 
     fn call(&self, request: Request) -> Self::Future {
-        let response = pipe_through(request, self.upstream_port);
-        futures::future::ok(response)
+        let uri = "http://drupal-8.localhost".parse().unwrap();
+
+        self.client.get(uri)
     }
 }
 
@@ -35,34 +38,20 @@ pub fn start_server(port: u16, upstream_port: u16) {
     let handle = core.handle();
     let http = Http::new();
     let listener = TcpListener::bind(&addr, &handle).unwrap();
+    let client = Client::new(&handle);
 
     let server = listener
         .incoming()
         .for_each(move |(sock, addr)| {
-                      http.bind_connection(&handle, sock, addr, Proxy { upstream_port });
-                      Ok(())
-                  });
+            http.bind_connection(&handle,
+                                 sock,
+                                 addr,
+                                 Proxy {
+                                     upstream_port: upstream_port,
+                                     client: client.clone(),
+                                 });
+            Ok(())
+        });
 
     core.run(server).unwrap();
-}
-
-fn pipe_through(request: Request, upstream_port: u16) -> Response {
-    let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
-
-    let uri = "http://drupal-8.localhost".parse().unwrap();
-
-    let work = client.get(uri);
-
-    match core.run(work) {
-        // Directly pass through the client response.
-        Ok(response) => response,
-        Err(_) => {
-            let mut response = Response::new();
-            response.set_status(StatusCode::InternalServerError);
-            response.set_body("error");
-
-            response
-        }
-    }
 }
