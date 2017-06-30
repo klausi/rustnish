ca24ac30bcae40983376d61cf1428721f4b583ce
Klaus Purer <klaus.purer@gmail.com>
2017-06-30 17:20:25 +0200

    Create an explcit tokio core when starting the server

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use hyper::Client;
 use hyper::server::{Http, Request, Response, Service};
 use tokio_core::reactor::Core;
 use hyper::StatusCode;
+use tokio_core::net::TcpListener;
+use futures::Stream;
 
 struct Proxy {
     upstream_port: u16,
@@ -27,10 +29,21 @@ pub fn start_server(port: u16, upstream_port: u16) {
     let address = "127.0.0.1:".to_owned() + &port.to_string();
     println!("Listening on {}", address);
     let addr = address.parse().unwrap();
-    let server = Http::new()
-        .bind(&addr, move || Ok(Proxy { upstream_port }))
-        .unwrap();
-    server.run().unwrap();
+
+    // Prepare a Tokio core that we will use for our server and our client.
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let http = Http::new();
+    let listener = TcpListener::bind(&addr, &handle).unwrap();
+
+    let server = listener
+        .incoming()
+        .for_each(move |(sock, addr)| {
+                      http.bind_connection(&handle, sock, addr, Proxy { upstream_port });
+                      Ok(())
+                  });
+
+    core.run(server).unwrap();
 }
 
 fn pipe_through(request: Request, upstream_port: u16) -> Response {
