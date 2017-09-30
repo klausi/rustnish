edbfc879b42d253a077b690d252ec386322cbbdd
Klaus Purer <klaus.purer@gmail.com>
2017-09-30 18:38:18 +0200

    fix(hyper): Fix memory leak in Hyper by upgrading it

--- tests/memory_leaks.rs
+++ tests/memory_leaks.rs
@@ -4,14 +4,15 @@ extern crate procinfo;
 extern crate rustnish;
 extern crate tokio_core;
 
-use hyper::{Client, Uri};
+use hyper::{Client, Method, Request, Uri};
 use futures::Future;
 use futures::future::join_all;
 use tokio_core::reactor::Core;
 
 mod common;
 
-// Tests that process memory does not excessively rise after 1000 requests.
+// Tests that process memory does not excessively rise after 1000 HTTP 1.0
+// requests.
 #[test]
 fn test_memory_after_1000_requests() {
     let port = common::get_free_port();
@@ -33,11 +34,13 @@ fn test_memory_after_1000_requests() {
 
     // Perform 10,000 requests in batches of 100. Otherwise we get "Too many open
     // files" errors because of opening too many ports.
-    for _i in 1..100 {
+    for _i in 1..1000 {
         let mut requests = Vec::new();
 
-        for _j in 0..100 {
-            requests.push(client.get(url.clone()).and_then(Ok));
+        for _j in 1..100 {
+            let mut request = Request::new(Method::Get, url.clone());
+            request.set_version(hyper::HttpVersion::Http10);
+            requests.push(client.request(request).and_then(Ok));
         }
         let work = join_all(requests);
         core.run(work).unwrap();
@@ -46,7 +49,7 @@ fn test_memory_after_1000_requests() {
     let memory_after = procinfo::pid::statm_self().unwrap().resident;
     // Allow memory to grow by 2MB, but not more.
     assert!(
-        memory_after < memory_before + 2048,
+        memory_after < memory_before + 4096,
         "Memory usage at server start is {}KB, memory usage after 10,000 requests is {}KB",
         memory_before,
         memory_after
