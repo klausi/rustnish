d3d09c05f87fd2fcba16c8f3375b00c7112b10fc
Klaus Purer <klaus.purer@gmail.com>
2017-09-30 13:40:00 +0200

    tests(memory): Allow memory usage to grow by 2MB

--- tests/memory_leaks.rs
+++ tests/memory_leaks.rs
@@ -31,7 +31,7 @@ fn test_memory_after_1000_requests() {
     // up space in RAM.
     let memory_before = procinfo::pid::statm_self().unwrap().resident;
 
-    // Perform 10000 requests in batches of 100. Otherwise we get "Too many open
+    // Perform 10,000 requests in batches of 100. Otherwise we get "Too many open
     // files" errors because of opening too many ports.
     for _i in 1..100 {
         let mut requests = Vec::new();
@@ -44,10 +44,10 @@ fn test_memory_after_1000_requests() {
     }
 
     let memory_after = procinfo::pid::statm_self().unwrap().resident;
-    // Allow memory to grow 10x, which is is still in the range of kilobytes.
+    // Allow memory to grow by 2MB, but not more.
     assert!(
-        memory_after < memory_before * 10,
-        "Memory usage at server start is {}KB, memory usage after 1000 requests is {}KB",
+        memory_after < memory_before + 2048,
+        "Memory usage at server start is {}KB, memory usage after 10,000 requests is {}KB",
         memory_before,
         memory_after
     );
