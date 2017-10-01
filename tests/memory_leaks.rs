7875ba4b471393cef22630388918a5902d5ef5a6
Klaus Purer <klaus.purer@gmail.com>
2017-10-01 14:08:17 +0200

    tests(memory): Improve parallel requests

--- tests/memory_leaks.rs
+++ tests/memory_leaks.rs
@@ -5,8 +5,7 @@ extern crate rustnish;
 extern crate tokio_core;
 
 use hyper::{Client, Method, Request, Uri};
-use futures::Future;
-use futures::future::join_all;
+use futures::future::{join_all, loop_fn, Future, Loop};
 use tokio_core::reactor::Core;
 
 mod common;
@@ -32,26 +31,37 @@ fn test_memory_after_1000_requests() {
     // up space in RAM.
     let memory_before = procinfo::pid::statm_self().unwrap().resident;
 
-    // Perform 10,000 requests in batches of 100. Otherwise we get "Too many open
-    // files" errors because of opening too many ports.
-    for _i in 1..1000 {
-        let mut requests = Vec::new();
+    let nr_requests = 20000;
+    let concurrency = 4;
 
-        for _j in 1..100 {
+    let mut parallel = Vec::new();
+    for _i in 0..concurrency {
+        let requests_til_done = loop_fn(0, |counter| {
             let mut request = Request::new(Method::Get, url.clone());
             request.set_version(hyper::HttpVersion::Http10);
-            requests.push(client.request(request).and_then(Ok));
-        }
-        let work = join_all(requests);
-        core.run(work).unwrap();
+            client
+                .request(request)
+                .then(move |_| -> Result<_, hyper::Error> {
+                    if counter < (nr_requests / concurrency) {
+                        Ok(Loop::Continue(counter + 1))
+                    } else {
+                        Ok(Loop::Break(counter))
+                    }
+                })
+        });
+        parallel.push(requests_til_done);
     }
+
+    let work = join_all(parallel);
+    core.run(work).unwrap();
 
     let memory_after = procinfo::pid::statm_self().unwrap().resident;
     // Allow memory to grow by 2MB, but not more.
     assert!(
-        memory_after < memory_before + 4096,
-        "Memory usage at server start is {}KB, memory usage after 10,000 requests is {}KB",
+        memory_after < memory_before + 2048,
+        "Memory usage at server start is {}KB, memory usage after {} requests is {}KB",
         memory_before,
+        nr_requests,
         memory_after
     );
 }
