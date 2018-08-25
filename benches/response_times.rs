867a8f7efcc42bd4424491a85d8a70b083e3e778
Klaus Purer <klaus.purer@gmail.com>
2018-08-25 12:27:58 +0200

    refactor(bench): Start dummy server externally ot more objectively compare runtimes

--- benches/response_times.rs
+++ benches/response_times.rs
@@ -1,7 +1,9 @@
 #![feature(test)]
 
 // Performs various timing tests on Rustnish and Varnish.
-// Execute with `cargo bench`
+//
+// The backend service must be started with `cargo run --example hello`.
+//
 // Varnish must be running and configured to listen on port 6081. The backend
 // port must be set to 9091.
 // Example Varnish configuration in /etc/varnish/default.vcl:
@@ -17,6 +19,7 @@
 //    return (pass);
 // }
 // ```
+// Execute with `cargo bench`
 
 extern crate futures;
 extern crate hyper;
@@ -25,12 +28,8 @@ extern crate test;
 extern crate tokio_core;
 
 use futures::future::{join_all, loop_fn, Loop};
-use futures::{future, Future, Stream};
-use tokio_core::net::TcpListener;
-use tokio_core::reactor::{Core, Handle};
-
-use hyper::header::{ContentLength, ContentType};
-use hyper::server::{self, Service};
+use futures::{Future, Stream};
+use tokio_core::reactor::Core;
 
 #[bench]
 fn a_1_request(b: &mut test::Bencher) {
@@ -82,6 +81,7 @@ fn d_10_parallel_requests_varnish(b: &mut test::Bencher) {
 
 #[bench]
 fn e_100_parallel_requests(b: &mut test::Bencher) {
+    rustnish::start_server_background(9090, 9091).unwrap();
     bench_requests(b, 100, 10, 9090);
 }
 
@@ -93,6 +93,7 @@ fn e_100_parallel_requests_varnish(b: &mut test::Bencher) {
 
 #[bench]
 fn f_1_000_parallel_requests(b: &mut test::Bencher) {
+    rustnish::start_server_background(9090, 9091).unwrap();
     bench_requests(b, 1_000, 100, 9090);
 }
 
@@ -105,7 +106,6 @@ fn f_1_000_parallel_requests_varnish(b: &mut test::Bencher) {
 fn bench_requests(b: &mut test::Bencher, amount: u32, concurrency: u32, proxy_port: u16) {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    spawn_hello(&handle);
 
     let client = hyper::Client::new(&handle);
 
@@ -123,7 +123,7 @@ fn bench_requests(b: &mut test::Bencher, amount: u32, concurrency: u32, proxy_po
                         assert_eq!(
                             res.status(),
                             hyper::StatusCode::Ok,
-                            "Varnish did not return a 200 HTTP status code. Make sure Varnish is configured on port {} and the backend port is set to 9091 in /etc/varnish/default.vcl",
+                            "Varnish did not return a 200 HTTP status code. Make sure Varnish is configured on port {} and the backend port is set to 9091 in /etc/varnish/default.vcl. Make sure the backend server is running with cargo run --example hello.",
                             proxy_port
                         );
                         // Read response body until the end.
@@ -143,46 +143,4 @@ fn bench_requests(b: &mut test::Bencher, amount: u32, concurrency: u32, proxy_po
         let work = join_all(parallel);
         core.run(work).unwrap();
     });
-}
-
-static PHRASE: &'static [u8] = b"Hello, World!";
-
-#[derive(Clone, Copy)]
-struct Hello;
-
-impl Service for Hello {
-    type Request = server::Request;
-    type Response = server::Response;
-    type Error = hyper::Error;
-    type Future = future::FutureResult<Self::Response, hyper::Error>;
-    fn call(&self, _req: Self::Request) -> Self::Future {
-        future::ok(
-            server::Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_header(ContentType::plaintext())
-                .with_body(PHRASE),
-        )
-    }
-}
-
-// Start a simple hello server on port 9091. This will be our upstream backend
-// for testing.
-fn spawn_hello(handle: &Handle) {
-    let addr = "127.0.0.1:9091".parse().unwrap();
-    let listener = TcpListener::bind(&addr, handle).unwrap();
-
-    let handle2 = handle.clone();
-    let http = hyper::server::Http::<hyper::Chunk>::new();
-    handle.spawn(
-        listener
-            .incoming()
-            .for_each(move |(socket, _addr)| {
-                handle2.spawn(
-                    http.serve_connection(socket, Hello)
-                        .map(|_| ())
-                        .map_err(|_| ()),
-                );
-                Ok(())
-            }).then(|_| Ok(())),
-    );
 }
